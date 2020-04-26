c3dbef69021c6ee34b872f6c507cf016e5b0485f
AIDARXAN <32789734+AIDARXAN@users.noreply.github.com>
2020-04-26 16:46:46 +0600

    Feature/vs 42 api unit test (#3)
    * UPDATED readme
    
    * ADDED test for 'api/v1/categories' endpoint

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+import unittest
 
-# Create your tests here.
+from django.test import Client
+
+
+class CategoriesEndpointTest(unittest.TestCase):
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_api_v1_categories(self):
+        response = self.client.get('/api/v1/categories/')
+        self.assertEqual(response.status_code, 200)
